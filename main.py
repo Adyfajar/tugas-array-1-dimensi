049788e2f1391e77f71983f07c68dfa9b3e9d300
Adyfajar <119859645+Adyfajar@users.noreply.github.com>
2023-03-19 11:43:37 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+print("array 1 dimensi")
+buah=(['apple','banana','papaya','orange'])
+#menggunakan variable "buah" untuk memanggil semuanya,
+#menggunakan variable "(buah[1)"untuk memanggil nomer urut 2
+print (buah[0])
